1b63aba54170e2510f0511eb37b60bfe89136406
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2023-09-14 10:14:55 +0200

    spawn scale cassé

--- Assets/Scripts/AI/AIStateSpawn.cs
+++ Assets/Scripts/AI/AIStateSpawn.cs
@@ -13,37 +13,36 @@ namespace AI
         public override AIController Controller { get; set; }
         public override AIState State { get; set; } = AIState.Spawn;
 
-        public float timer;
-    
-        public override void Update()
-        {
-            //timer avec animation de spawn puis passage au walk
-            Controller.EnemyTransform.localScale = Vector3.zero;
-            Controller.gameObject.transform.DOScale(1, timer);
+        private float _timer;
 
-            Debug.Log(timer);
-
-            if(timer <= 0)
-            {
-                Controller.SetAIState(AIState.Walk);
-            }
-            else
-            {
-                //timer -= Time.deltaTime;
-            }
-        }
 
         public override void Start()
         {
-            timer = Controller.Data.SpawnTimer;
+            //timer avec animation de spawn puis passage au walk
+            Controller.EnemyTransform.localScale = Vector3.zero;
+            Controller.gameObject.transform.DOScale(1, _timer);
+
+            _timer = Controller.Data.SpawnTimer;
 
             if (Controller.EnemyAnimator != null)
             {
                 Controller.EnemyAnimator.SetBool("idleAI", true);
             }
-            
+
             Controller.SetAIState(AIState.Walk);
         }
+
+        public override void Update()
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                //Controller.SetAIState(AIState.Walk);
+                Debug.Log("timer");
+            }
+        }
+
 
         public override void End()
         {
